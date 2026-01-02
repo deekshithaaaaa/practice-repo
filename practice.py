2a3c733fbac0e8105b6c873ead09e198a2b47926
Deekshitha <deekshithanallamothu18@gmail.com>
2026-01-02 21:58:53 +0530

    commited practice file

--- practice.py
+++ practice.py
@@ -1,8 +1,13 @@
-def pairs_with_sum(nums, target):
-    for i in range(len(nums)):
-        for j in range(i+1,len(nums)):
-            if nums[i] +nums[j]== target:
-                print((nums[i],nums[j]))
-            
-    return None
-print(pairs_with_sum( nums = [1, 2, 3, 4, 5], target = 6))
+num1 = int(input("enter a num1: "))
+num2 = int(input("enter a num2: "))
+num3 = int(input("enter a num3: "))
+num4 = int(input("enter a num4: "))
+num5 = int(input("enter a num5: "))
+lst = [num1,num2,num3,num4,num5]
+largest = max(lst)
+print(largest)
+
+for index,i in enumerate(lst):
+    print(index,i)
+print("even") if largest%2==0 else print("odd")
+
